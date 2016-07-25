dd50f6fab8dbc6dcff5d9b7d73f637d9f3f9569e
Dayne Wright <daynewr@gmail.com>
2016-07-25 15:57:04 -0500

    added SPA functionality and styling for form

---
+++ nav.js
@@ -0,0 +1,32 @@
+(function () {
+
+  showPage(location.hash);
+
+  // You can nest IIFES for additional segmentation
+  // and also name IIFEs for additional context
+  ;(function addNavLinkEvents () {
+    var navLinks = document.querySelectorAll('li');
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].addEventListener('click', clickedNavLink);
+    }
+  }())
+
+  function clickedNavLink (evt) {
+    var pages = document.querySelectorAll('.page');
+
+    for (var i = 0; i < pages.length; i++) {
+      pages[i].classList.add('hidden');
+    }
+
+    showPage(evt.target.hash);
+  }
+
+  function showPage (id) {
+    if (id) {
+      document.querySelector(id).classList.remove('hidden');
+    } else {
+      document.querySelector('#home').classList.remove('hidden');
+    }
+  }
+
+}())
